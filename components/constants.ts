00dc1a1d99b9ec596ae3cfdabd5cfe9c2ead0a34
ErnestaP <ernesta.petraityte@yahoo.com>
2020-03-30 18:01:51 +0300

    plots size

---
+++ components/constants.ts
@@ -0,0 +1,37 @@
+export const sizes = {
+  tiny: {
+    label: 'Tiny',
+    size: {
+      w: 67,
+      h: 50,
+    },
+  },
+  small: {
+    label: 'Small',
+    size: {
+      w: 133,
+      h: 100,
+    },
+  },
+  medium: {
+    label: 'Medium',
+    size: {
+      w: 266,
+      h: 200,
+    },
+  },
+  large: {
+    label: 'Large',
+    size: {
+      w: 532,
+      h: 400,
+    },
+  },
+  fill: {
+    label: 'Fill',
+    size: {
+      w: 720,
+      h: 541,
+    },
+  },
+}
