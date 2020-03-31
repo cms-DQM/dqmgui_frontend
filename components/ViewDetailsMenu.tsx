9addc1a152fb74318dda291d34ecf4f0f1e89390
ErnestaP <ernesta.petraityte@yahoo.com>
2020-03-31 15:25:14 +0300

    added apis, add/remove runs forom viewDetails menu

--- components/ViewDetailsMenu.tsx
+++ components/ViewDetailsMenu.tsx
@@ -1,23 +1,18 @@
 
 import { useState, ChangeEvent, FormEvent } from 'react'
-
+import { v4 as uuidv4 } from 'uuid';
 import { field_name } from './constants'
-
-export interface TrinomialProps {
-  [key: string]: string | number,
-}
+import { TrinomialProps } from '../containers/display/interfaces';
 
 interface ViewDetailsMenuProps {
   set_plot_to_overlay: any
 }
 
 export const ViewDetailsMenu = ({ set_plot_to_overlay }: ViewDetailsMenuProps) => {
+  const id = uuidv4()
 
   const [trinomials, change_trinomial_values] = useState<TrinomialProps[]>(
-    [{ id: 1, dataset_name: '', run_number: NaN, label: '' },
-    { id: 2, dataset_name: '', run_number: NaN, label: '' },
-    { id: 3, dataset_name: '', run_number: NaN, label: '' },
-    { id: 4, dataset_name: '', run_number: NaN, label: '' }])
+    [{ id: id, dataset_name: '', run_number: NaN, label: '' }])
 
 
   const change_value = (value: string, key: string, id: string | number) => {
@@ -28,6 +23,20 @@ export const ViewDetailsMenu = ({ set_plot_to_overlay }: ViewDetailsMenuProps) =
     copy[index_of_line] = current_line
     change_trinomial_values(copy)
   };
+
+  const addRun = () => {
+    const copy: TrinomialProps[] = [...trinomials]
+    const id = uuidv4()
+    const newRun = { id: id, dataset_name: '', run_number: NaN, label: '' }
+    copy.push(newRun)
+    change_trinomial_values(copy)
+  }
+
+  const removeRun = (id: string | number) => {
+    const copy: TrinomialProps[] = [...trinomials]
+    const removed = copy.filter((run: TrinomialProps) => run.id !== id)
+    change_trinomial_values(removed)
+  }
 
   const filter_valid_runs = (trinomials: TrinomialProps[]) => trinomials.filter((trinomial: TrinomialProps) => {
     if (trinomial.run_number) {
@@ -42,9 +51,9 @@ export const ViewDetailsMenu = ({ set_plot_to_overlay }: ViewDetailsMenuProps) =
       set_plot_to_overlay(filtered)
     }}>
       {
-        trinomials.map((line: TrinomialProps) =>
-          <div id={(line.id).toString()}>
-            {Object.keys(line).map((field: string) => {
+        trinomials.map((trinomial: TrinomialProps) =>
+          <div id={(trinomial.id).toString()}>
+            {Object.keys(trinomial).map((field: string) => {
               if (field !== 'id') {
                 return (
                   <p>
@@ -54,16 +63,26 @@ export const ViewDetailsMenu = ({ set_plot_to_overlay }: ViewDetailsMenuProps) =
                       type='text'
                       name={field}
                       onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                        change_value(e.target.value, field, line.id)
+                        change_value(e.target.value, field, trinomial.id)
                       } />
                   </p>
                 )
               }
             }
             )}
+             <button onClick={() => {
+              if (trinomials.length > 1) {
+                removeRun(trinomial.id)
+              }
+            }}>Remove</button>
           </div>
         )
       }
+      <button onClick={() => {
+        if (trinomials.length < 4) {
+          addRun()
+        }
+      }}>Add</button>
       <button type="submit">Submit</button>
     </form>
   )
