5ecbfb3a869381e805a6ec74f8f0487212fac93b
ErnestaP <ernesta.petraityte@yahoo.com>
2020-04-08 19:47:05 +0300

    style for primary table and folders

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
 import React, { FC, ChangeEvent, Dispatch, FormEvent, useReducer } from 'react';
+import { Form, Input } from 'antd';
 
 import {
   navReducer,
@@ -6,6 +7,7 @@ import {
   setSearchFieldByDatasetName,
   setSearchFieldByRunNumber,
 } from '../reducers/navReducer';
+import { StyledButton, StyledFormItem } from './styles'
 
 interface NavProps {
   setRunNumber: Dispatch<any>;
@@ -15,33 +17,60 @@ interface NavProps {
 const Nav: FC<NavProps> = ({ setRunNumber, setDatasetName }) => {
   const [state, dispatch] = useReducer(navReducer, initialState);
 
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 0, span: 4 },
+  };
   return (
-    <form
-      onSubmit={(e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    <Form
+      layout={'inline'}
+      {...layout}
+      name="search_form"
+      className="fieldLabel"
+      initialValues={{ remember: true }}
+      onFinish={() => {
         setRunNumber(state.search_by_run_number);
         setDatasetName(state.search_by_dataset_name);
       }}
+    // onFinishFailed={onFinishFailed}
     >
-      <label htmlFor="run_number">Run number:</label>
-      <input
-        id="run_number"
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setSearchFieldByRunNumber(e.target.value)(dispatch)
-        }
-        type="text"
+      <StyledFormItem
+        label="Run number:"
         name="run_number"
-      />
-      <label htmlFor="dataset_name">Dataset name:</label>
-      <input
-        id="dataset_name"
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setSearchFieldByDatasetName(e.target.value)(dispatch)
-        }
-        type="text"
-      />
-      <button type="submit">Search</button>
-    </form>
+      >
+        <Input
+          id="run_number"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setSearchFieldByRunNumber(e.target.value)(dispatch)
+          }
+          type="text"
+          name="run_number"
+        />
+      </StyledFormItem>
+      <StyledFormItem
+        label="Dataset name:"
+        name="dataset_name"
+      >
+        <Input
+          id="dataset_name"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setSearchFieldByDatasetName(e.target.value)(dispatch)
+          }
+          type="text"
+        />
+      </StyledFormItem>
+      <Form.Item {...tailLayout}>
+        <StyledButton
+          type="primary"
+          htmlType="submit"
+        >
+          Search
+        </StyledButton>
+      </Form.Item>
+    </Form>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
+import { Layout } from 'antd';
+import "antd/dist/antd.css";
 
 import Nav from '../components/Nav';
 import SearchResults from '../containers/search/SearchResults';
 import DiplayFolders from '../containers/display/DisplayFolderAndPlot';
-import { useState } from 'react';
 import { useSearch } from '../hooks/useSearch';
-import "./styles.scss";
+import { StyledHeader, StyledLayout, StyledContent } from './styles'
+
+const { Header, Footer, Sider, Content } = Layout;
 
 interface FolderPathQuery {
   run_number?: number;
@@ -23,7 +27,7 @@ const Index: NextPage<FolderPathQuery> = (query) => {
     dataset_name
   );
   return (
-    <div className="Header">
+    <div >
       <Head>
         <script
           crossOrigin="anonymous"
@@ -31,22 +35,32 @@ const Index: NextPage<FolderPathQuery> = (query) => {
           src="/jsroot-5.8.0/scripts/JSRootCore.js?2d&hist&more2d"
         ></script>
       </Head>
-      <Nav setRunNumber={setRunNumber} setDatasetName={setDatasetName} />
-      {query.run_number && query.dataset_name ? (
-        <DiplayFolders
-          run_number={query.run_number}
-          dataset_name={query.dataset_name}
-          folder_path={query.folder_path || ''}
-        />
-      ) : searching ? (
-        <SearchResults
-          isLoading={isLoading}
-          results={results}
-          results_grouped={results_grouped}
-        />
-      ) : (
-            <div>Home</div>
-          )}
+      <StyledLayout>
+        <StyledHeader>
+          <Nav
+            setRunNumber={setRunNumber}
+            setDatasetName={setDatasetName}
+          />
+        </StyledHeader>
+        <StyledContent>
+          {query.run_number && query.dataset_name ? (
+            <DiplayFolders
+              run_number={query.run_number}
+              dataset_name={query.dataset_name}
+              folder_path={query.folder_path || ''}
+            />
+          ) : searching ? (
+            <SearchResults
+              isLoading={isLoading}
+              results={results}
+              results_grouped={results_grouped}
+            />
+          ) : (
+                <div>Home</div>
+              )}
+        </StyledContent>
+        <Footer>Footer</Footer>
+      </StyledLayout>
     </div>
   );
 };
