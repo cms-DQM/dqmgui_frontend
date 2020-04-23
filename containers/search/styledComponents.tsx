6efcd57c9f65872facea87e4fafde00b3e93e2c0
Ernesta Petraityte <ernestaemilijapetraityte@gmail.com>
2020-04-24 02:14:58 +0300

    spinner style

--- containers/search/styledComponents.tsx
+++ containers/search/styledComponents.tsx
@@ -17,7 +17,8 @@ export const StyledWrapper = styled.div`
 export const Spinner = () => <Spin tip="Loading..." />;
 
 export const SpinnerWrapper = styled.div`
-  height: 100%;
+  height: 80vh;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
