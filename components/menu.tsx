0f0d76d7e1d4d3a0f6ab407129b0eabeade6ccec
ErnestaP <ernesta.petraityte@yahoo.com>
2020-05-18 17:01:26 +0300

    a style

--- components/menu.tsx
+++ components/menu.tsx
@@ -32,7 +32,7 @@ export const DropdownMenu = ({ options, defaultValue, action }: MenuProps) => {
     <Row>
       <Col>
         <Dropdown overlay={plotMenu(options, defaultValue)} trigger={['hover']}>
-          <a>
+          <a style={{color: 'white'}}>
             {value.label} <DownOutlined />{' '}
           </a>
         </Dropdown>
