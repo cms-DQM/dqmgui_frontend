8e83b9f808d98d11cad0ca9ce8bb43c7ba1a6f7e
Ernesta Petraityte <ernestaemilijapetraityte@gmail.com>
2020-05-22 13:12:02 +0300

    srcoll down

--- components/plots/zoomedPlots/index.tsx
+++ components/plots/zoomedPlots/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import {
 
   PlotDataProps,
@@ -9,6 +9,7 @@ import { ZoomedPlots as ZoomedOverlaidPlots } from './zoomedOverlayPlots';
 import { ZoomedPlots as ZoomedPlotsWithoutOverlay } from './zoomedPlots';
 import { ViewDetailsMenu } from './viewDetails';
 import { useRouter } from 'next/router';
+import { store } from '../../../contexts/leftSideContext';
 
 interface ZoomedPlotsProps {
   selected_plots: PlotDataProps[];
@@ -19,10 +20,18 @@ export const ZoomedPlots = ({
 }: ZoomedPlotsProps) => {
   const router = useRouter();
   const query: QueryProps = router.query;
+  const globalState = useContext(store)
+
+  const { setImageRefScrollDown } = globalState
+
+  const rightSideRef = useRef(null)
+  useEffect(() => {
+    setImageRefScrollDown(rightSideRef)
+  }, [])
 
   const overlay_plot = query.overlay_data
   return (
-    <div style={{ width: '100%' }} >
+    <div style={{ width: '100%', overflow: 'scroll' }} ref={rightSideRef}>
       <ViewDetailsMenu />
       {overlay_plot ? (
         <ZoomedOverlaidPlots
