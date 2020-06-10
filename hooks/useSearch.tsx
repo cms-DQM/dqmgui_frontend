677f78d0a740d2e1dded1a527a03c45c0bc054a9
ErnestaP <ernesta.petraityte@yahoo.com>
2020-06-10 12:35:41 +0300

    results.reduce ?  results.reduce : []

--- hooks/useSearch.tsx
+++ hooks/useSearch.tsx
@@ -33,10 +33,10 @@ export const useSearch = (
     };
   }
   const { samples: results }: { samples: any[] } = data;
-  const parsed_results = results.reduce(
+  const parsed_results = results ? results.reduce(
     (prev, { items }) => [...prev, ...items],
     []
-  );
+  ) : [];
 
   const results_grouped = _.chain(parsed_results)
     .sortBy('dataset')
