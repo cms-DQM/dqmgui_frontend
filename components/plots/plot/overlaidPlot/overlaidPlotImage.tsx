ae5539b76c2411a53a32482cbbe1c2d79244e5b6
Ernesta Petraityte <ernestaemilijapetraityte@gmail.com>
2020-07-16 15:43:48 +0300

    normlaize

--- components/plots/plot/overlaidPlot/overlaidPlotImage.tsx
+++ components/plots/plot/overlaidPlot/overlaidPlotImage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useRouter } from 'next/router';
 
 import { root_url } from '../../../../config/config';
@@ -25,6 +25,7 @@ import {
   addPlotToRightSide,
   scrollToBottom,
 } from '../singlePlot/utils';
+import { store } from '../../../../contexts/leftSideContext';
 
 interface OverlaidPlotImageProps {
   params_for_api: ParamsForApiProps;
@@ -39,7 +40,12 @@ export const OverlaidPlotImage = ({
   isPlotSelected,
   imageRefScrollDown,
 }: OverlaidPlotImageProps) => {
+  const globalState = useContext(store);
+  const { normalize } = globalState;
+
   params_for_api.plot_name = plot.name;
+  params_for_api.normalize = normalize
+
   const overlaid_plots_urls = get_overlaied_plots_urls(params_for_api);
   const joined_overlaid_plots_urls = overlaid_plots_urls.join('');
   params_for_api.joined_overlaied_plots_urls = joined_overlaid_plots_urls;
@@ -59,21 +65,21 @@ export const OverlaidPlotImage = ({
           minheight={params_for_api.height}
           width={params_for_api.width?.toString()}
           is_plot_selected={isPlotSelected.toString()}
-          // report={plot.properties.report}
+        // report={plot.properties.report}
         >
           <PlotNameCol>{plot.displayedName}</PlotNameCol>
           <Column>
             {isPlotSelected ? (
               <MinusIcon onClick={() => removePlotFromRightSide(query, plot)} />
             ) : (
-              <PlusIcon
-                onClick={async () => {
-                  await addPlotToRightSide(query, plot);
-                  scroll(imageRef);
-                  scrollToBottom(imageRefScrollDown);
-                }}
-              />
-            )}
+                <PlusIcon
+                  onClick={async () => {
+                    await addPlotToRightSide(query, plot);
+                    scroll(imageRef);
+                    scrollToBottom(imageRefScrollDown);
+                  }}
+                />
+              )}
           </Column>
           <div
             onClick={async () => {
