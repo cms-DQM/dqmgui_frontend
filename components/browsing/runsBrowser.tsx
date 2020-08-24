5a25dbcb6233ea40f4189cbaa604d448ac04ba09
ErnestaP <ernesta.petraityte@yahoo.com>
2020-08-24 16:25:18 +0300

    run parsing

--- components/browsing/runsBrowser.tsx
+++ components/browsing/runsBrowser.tsx
@@ -35,7 +35,7 @@ export const RunBrowser = ({
 
   const { results_grouped, isLoading } = useSearch('', query.dataset_name);
 
-  const runNumbers = results_grouped[0] ? results_grouped[0].runs : []
+  const runNumbers = results_grouped[0] ? results_grouped[0].runs.map((run: number) => run.toString()) : []
 
   useEffect(() => {
     const query_run_number = query.run_number ? query.run_number : '';
