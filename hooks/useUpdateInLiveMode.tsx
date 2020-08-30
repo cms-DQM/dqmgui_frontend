527a0a8220a259c0e3956618dc8865cb3afceaa7
ErnestaP <ernesta.petraityte@yahoo.com>
2020-08-30 13:44:17 +0300

    functions_config.new_back_end.new_back_end value must be true in order to update in live mode

--- hooks/useUpdateInLiveMode.tsx
+++ hooks/useUpdateInLiveMode.tsx
@@ -15,9 +15,9 @@ export const useUpdateLiveMode = (errors: any[]) => {
   const online_mode = functions_config.mode === 'ONLINE'
   // latest runs list should be updated when online mode is on
   //!query.search_dataset_name && !query.run_number because latest runs list is visible in inital page (when no data is selected)
-  const latest_runs_list = online_mode && functions_config.new_back_end.latest_runs && !query.search_dataset_name && !query.search_run_number && errors.length === 0
+  const latest_runs_list = online_mode && functions_config.new_back_end.latest_runs && functions_config.new_back_end.new_back_end && !query.search_dataset_name && !query.search_run_number && errors.length === 0
   // live mode - it's when dataset is  "/Global/Online/ALL" and run number is "0"
-  const live_mode = query.run_number === "0" && query.dataset_name === "/Global/Online/ALL" && online_mode && errors.length === 0
+  const live_mode = query.run_number === "0" && query.dataset_name === "/Global/Online/ALL" && online_mode && errors.length === 0 && functions_config.new_back_end.new_back_end
   //not_older_than seconds should be reachtable globaly 
   const { set_updated_by_not_older_than } = React.useContext(store);
 
