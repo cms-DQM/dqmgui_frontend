7c97b05805472e2eedfd8fdee522b77e6e48bc49
ErnestaP <ernesta.petraityte@yahoo.com>
2021-02-15 14:54:53 +0100

    console.log

--- containers/display/utils.ts
+++ containers/display/utils.ts
@@ -197,7 +197,7 @@ export const getChangedQueryParams = (
 export const changeRouter = (parameters: ParsedUrlQueryInput) => {
   const stringified = qs.stringify(parameters, {});
   const url_which_is_visible = root_url_ !== '/' ? `${root_url_}/?${stringified}` : undefined
-  console.log(root_url_)
+
   Router.push({
     pathname: '',
     query: parameters,
