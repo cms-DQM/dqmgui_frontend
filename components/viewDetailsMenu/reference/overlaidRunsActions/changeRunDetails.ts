6550efc2840682611bf99261048e4e0319ea37bb
ErnestaP <ernesta.petraityte@yahoo.com>
2021-03-15 15:05:23 +0100

    checkboxes next to overlaid plots

--- components/viewDetailsMenu/reference/overlaidRunsActions/changeRunDetails.ts
+++ components/viewDetailsMenu/reference/overlaidRunsActions/changeRunDetails.ts
@@ -2,15 +2,16 @@ import { QueryProps, TripleProps } from "../../../../containers/display/interfac
 import { changeRouter, getChangedQueryParams } from "../../../../containers/display/utils";
 import { addOverlayData } from "../../../plots/plot/singlePlot/utils";
 
-export const change_run_details =  (runs: TripleProps[]) => async(setTriples: ((run: TripleProps[])=> void), query: QueryProps) => {
-    await changeRouter(
-      getChangedQueryParams(
-        {
-          overlay_data: `${addOverlayData(runs)}`,
-        },
-        query
-      )
-    );
-    setTriples(runs);
-  };
+export const change_run_details = (runs: TripleProps[]) => async (setTriples: ((run: TripleProps[]) => void), query: QueryProps) => {
+const only_checked_runs = runs.filter((run) => run.checked)
+  await changeRouter(
+    getChangedQueryParams(
+      {
+        overlay_data: `${addOverlayData(only_checked_runs)}`,
+      },
+      query
+    )
+  );
+  setTriples(runs);
+};
 
