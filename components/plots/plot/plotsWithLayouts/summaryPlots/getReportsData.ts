2085544aafed8f1623189075ab8c37cbe537649e
ErnestaP <ernesta.petraityte@yahoo.com>
2021-07-07 15:19:18 +0200

    scroll on report Modal

--- components/plots/plot/plotsWithLayouts/summaryPlots/getReportsData.ts
+++ components/plots/plot/plotsWithLayouts/summaryPlots/getReportsData.ts
@@ -1,3 +1,5 @@
+import { pathOr } from 'ramda'
+
 import { get_all_plots_and_folders } from '../../../../../containers/display/content/quickCollectionPlotsAndFoldersHandling/get_all_plots_and_folders';
 import { getReportInfo } from './getReportInfo';
 
@@ -15,7 +17,7 @@ export const getReportsData = async (props: getReportsDataProps) => {
     const folder_path = `${subsystem}/EventInfo`
     const parameters = { folder_path, run_number, dataset_name, notOlderThan }
     const response = await get_all_plots_and_folders(parameters)
-    const { plots } = response
+    const plots = pathOr([], ['plots'], response)
     const plots_names = plots.map((plot) => plot.name)
     const data = await getReportInfo({ run_number, dataset_name, lumi, subsystem, plots_names })
     return data
--- components/plots/plot/plotsWithLayouts/summaryPlots/styledComponents.tsx
+++ components/plots/plot/plotsWithLayouts/summaryPlots/styledComponents.tsx
@@ -4,8 +4,13 @@ import styled from 'styled-components';
 import { theme } from '../../../../../styles/theme';
 
 export const Report_summary_modal = styled(Modal)`
-.ant-modal-content{
-    overlfow: scroll;
+.ant-modal-body{
+    height: 80vh;
+    overflow: scroll;
+};
+.ant-list ant-list-split{
+    height: 100%;
+    
 }
 `
 export const Report_summary_button = styled(Button)`
